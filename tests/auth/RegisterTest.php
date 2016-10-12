e4c0f5ca35af5814214d189c457e8387d306203c
Vincent Chi <song374561@gmail.com>
2016-10-12 20:54:30 +0800

    Create register test.

---
+++ tests/auth/RegisterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class RegisterTest extends TestCase
+{
+    use DatabaseToTest;
+
+    public function testRegisterPage()
+    {
+        $this->visit('/register')
+             ->seePageIs('/register')
+             ->see('註冊');
+    }
+
+    public function testRegisterSuccess()
+    {
+        $user = factory(\Xenex\User::class)->make([
+            'password' => bcrypt($password = str_random(20)),
+        ]);
+
+        $this->visit('/register')
+             ->type($user->email, 'email')
+             ->type($user->name, 'name')
+             ->type($password, 'password')
+             ->type($password, 'password_confirmation')
+             ->press('註冊');
+
+        $this->seeInDatabase('users', [
+            'email' => $user->email,
+            'name' => $user->name,
+        ]);
+
+        $this->assertTrue(\Hash::check($password, \Xenex\User::where('email', $user->email)->first()->password));
+    }
+}
