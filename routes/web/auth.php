d9cb0c1e89a986d1c09c2885eaf0d3ff012d098b
Vincent Chi <song374561@gmail.com>
2016-10-14 14:02:30 +0800

    Routes and rebuild ForgotPasswordController.

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -7,7 +7,8 @@ Route::get('/login', 'Auth\LoginController@getLogin');
 Route::post('/login', 'Auth\LoginController@postLogin');
 Route::post('/logout', 'Auth\LogoutController@postLogout');
 
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset', 'Auth\ForgotPasswordController@getReset');
+Route::post('password/email', 'Auth\ForgotPasswordController@postEmail');
+
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
