9afd64a9407dfcce1476573d16bebf0765c09358
Vincent Chi <song374561@gmail.com>
2016-10-14 23:16:19 +0800

    Create account setting blade and hyperlink for user/account/setting.

--- app/Http/Controllers/User/Account/SettingController.php
+++ app/Http/Controllers/User/Account/SettingController.php
@@ -2,12 +2,29 @@
 
 namespace Xenex\Http\Controllers\User\Account;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
+use Illuminate\View\View;
 use Xenex\Http\Requests;
 use Xenex\Http\Controllers\Controller;
 
 class SettingController extends Controller
 {
-    //
+    protected $view = 'user.account.setting';
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function getSetting(): View
+    {
+        return view($this->view ?? 'user.account.setting');
+    }
+
+    public function postSetting(Request $request): RedirectResponse
+    {
+
+    }
 }
