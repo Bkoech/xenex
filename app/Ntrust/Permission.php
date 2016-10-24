0e3268c4d042c6322eadab2c1872e1bb4faaff21
Vincent Chi <song374561@gmail.com>
2016-10-24 13:53:10 +0800

    Create Role and Permission Model, add UserTrait for User model.

---
+++ app/Ntrust/Permission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Xenex\Ntrust;
+
+use Illuminate\Database\Eloquent\Model;
+use Klaravel\Ntrust\Traits\NtrustPermissionTrait;
+
+class Permission extends Model
+{
+    use NtrustPermissionTrait;
+
+    protected static $roleProfile = 'user';
+}
---
+++ app/Ntrust/Role.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Xenex\Ntrust;
+
+use Illuminate\Database\Eloquent\Model;
+use Klaravel\Ntrust\Traits\NtrustRoleTrait;
+
+class Role extends Model
+{
+    use NtrustRoleTrait;
+
+    protected static $roleProfile = 'user';
+}
